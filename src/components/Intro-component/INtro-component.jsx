74778a0634589ea038cfc5e971f87961ac285496
AmaanKhan2000 <amaankhxn2000@gmail.com>
2021-01-17 16:57:53 +0530

    Made the Main UI and the main page

---
+++ src/components/Intro-component/INtro-component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import Form from 'react-bootstrap/Form'
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+import Button from 'react-bootstrap/Button'
+
+import './Intro-component.css';
+
+const Intro = () => (
+  <div className="Z">
+    <div className="row">
+      <div className="col-md-6">
+      <Jumbotron className='border-ra bg-dark text-light mx-auto my-6' variant='bg-dark'>
+        <h5>The world is facing an unprecedented challenge with communities and economies everywhere affected by the growing COVID-19 pandemic. The Covid 19 Pandemic has affected us all in many different ways. For some it has been a life changing crisis. We as an organistaion want to join hands with you and help the people out there who are going through crisis. It’s never been more urgent to support the global response. On the right there is a form where you can fill in your details and donate money. Each penny is counted! </h5>
+
+      </Jumbotron>
+      </div>
+      <div className="col-md-6">
+      <Form className ='bg-info my-6 mx-auto px-5 py-5 Y'>
+      <Form.Group controlId="formBasicName">
+    <Form.Label>Name</Form.Label>
+    <Form.Control type="email" placeholder="Enter Name" />
+  </Form.Group>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPhoneNo.">
+    <Form.Label>Phone No.</Form.Label>
+    <Form.Control type="Number" placeholder="Enter Phone No." />
+  </Form.Group>
+  <p className='middle'>
+  <Button variant="success" className='p-2 px-5 mt-3 middle'>DONATE!</Button>
+   </p>
+</Form>
+      </div>
+    </div>
+
+  </div>
+);
+
+export default Intro;
---
+++ src/components/main-ui-component/main-ui-component.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './main-ui-component.styles.css';
+
+import CovidVideo from '../../Videos/CovidVideo.mp4';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+import Logo from '../../Videos/logo_transparent.png';
+
+import Intro from '../Intro-component/INtro-component';
+
+const MainUI = () => (
+  <div className="showcase">
+    <div className="video-container">
+      <video src={CovidVideo} autoPlay muted loop></video>
+    </div>
+    <div className="content">
+    <Navbar bg="light" variant="light" className='visibility'>
+  <Navbar.Brand href="#home" className='center'>
+    <img
+      alt=""
+      src={Logo}
+      width="90"
+      height="90"
+      className="d-inline-block align-top align-center ml-5"
+    />{'  '}
+  </Navbar.Brand>
+  <p className="downscale">COVID-19 RELIEF FUND</p>
+  </Navbar>
+  <Intro />
+  </div>
+  
+  </div>
+);
+
+export default MainUI; 
